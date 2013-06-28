d1b49b68f60248f32e64b27214f32cb0c2ca7c0a
lostleaf <jack951753@gmail.com>
2013-06-28 10:59:18 +0800

    finish user micropost

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,16 +1,21 @@
 class StaticPageController < ApplicationController
-  def home
-  end
+    
+    def home
+        if signed_in?
+            @micropost  = current_user.microposts.build
+            @feed_items = current_user.feed.paginate(page: params[:page])
+        end
+    end
 
-  def help
-  end
+    def help
+    end
 
-  def about
-      
-  end
+    def about
 
-  def contact
-      
-  end
+    end
+
+    def contact
+
+    end
 
 end
